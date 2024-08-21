20b9d3072f6f15682dccb91ca5b630620e6934d5
redek2 <128227165+redek2@users.noreply.github.com>
2024-08-22 00:48:23 +0200

    Add files via upload

---
+++ Kuba/Kuba/Program.cs
@@ -0,0 +1,31 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    { 
+
+        Console.WriteLine("Siema pedały\n");
+        string znaki = Console.ReadLine();
+        Console.WriteLine();
+        string poprawione = "";
+
+        foreach(char a in znaki)
+        {
+            if(char.IsLetter(a))
+            {
+                
+            }
+            else if(char.IsDigit(a))
+            {
+                poprawione += a;
+            }
+            else if(char.IsPunctuation(a))
+            {
+                
+            }
+        }
+
+        poprawione = poprawione + "_4";
+
+        Console.WriteLine(poprawione);
+    }
+}
